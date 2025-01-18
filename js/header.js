10c62cbd6638dadad6c2e1399502d40e8b8b6c3b
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2025-01-18 21:12:44 +0200

    Created toggle functionality for mobile menu elements

--- js/header.js
+++ js/header.js
@@ -1,8 +1,19 @@
 const burgerIcon = document.querySelector('#js-burger');
 const mobileMenuBox = document.querySelector('#js-mobile-menu-box');
+const mobileMenuLinks = mobileMenuBox.querySelectorAll('a');
 
-burgerIcon.addEventListener('click', () => {
+function toggleMenuElements() {
   document.body.classList.toggle('overflow-hidden');
   burgerIcon.classList.toggle('header__burger--open');
   mobileMenuBox.classList.toggle('header__mobile-menu-box--open');
+}
+
+burgerIcon.addEventListener('click', () => {
+  toggleMenuElements();
+});
+
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    toggleMenuElements();
+  });
 });
