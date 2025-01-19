4684826d83b2aa344c5eb42c05d2f3d15145a067
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2025-01-19 20:53:21 +0200

    Deleted listener

--- js/index.js
+++ js/index.js
@@ -1,20 +1,16 @@
 function init() {
+  const hash = window.location.hash;
+  if (hash) {
+    console.log('// Знаходимо елемент із відповідним ID');
+    const target = document.querySelector(hash);
+    if (target) {
+      console.log('// Виконуємо прокрутку до елемента');
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
   import('./header.js');
   import('./categories-aside.js');
-
-  document.addEventListener('DOMContentLoaded', () => {
-    console.log('// Отримуємо хеш із URL');
-    const hash = window.location.hash;
-    if (hash) {
-      console.log('// Знаходимо елемент із відповідним ID');
-      const target = document.querySelector(hash);
-      if (target) {
-        // Виконуємо прокрутку до елемента
-        console.log('// Виконуємо прокрутку до елемента');
-        target.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  });
 }
 
 const totalPartials = document.querySelectorAll(
