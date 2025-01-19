5ea615453aa1927621fccc1a3621fd40a28db637
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2025-01-19 20:47:43 +0200

    Add log for function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 function init() {
   import('./header.js');
   import('./categories-aside.js');
+
+  document.addEventListener('DOMContentLoaded', () => {
+    console.log('// Отримуємо хеш із URL');
+    const hash = window.location.hash;
+    if (hash) {
+      console.log('// Знаходимо елемент із відповідним ID');
+      const target = document.querySelector(hash);
+      if (target) {
+        // Виконуємо прокрутку до елемента
+        console.log('// Виконуємо прокрутку до елемента');
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  });
 }
 
 const totalPartials = document.querySelectorAll(
@@ -11,17 +25,4 @@ let loadedPartialsCount = 0;
 document.body.addEventListener('htmx:afterOnLoad', () => {
   loadedPartialsCount++;
   if (loadedPartialsCount === totalPartials) init();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Отримуємо хеш із URL
-  const hash = window.location.hash;
-  if (hash) {
-    // Знаходимо елемент із відповідним ID
-    const target = document.querySelector(hash);
-    if (target) {
-      // Виконуємо прокрутку до елемента
-      target.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
 });
