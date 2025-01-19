5c313393bf0a5575c865bc95df1da5415f5e8a3f
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2025-01-19 21:05:48 +0200

    Resolve problem with scrolling to id

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 function init() {
+  import('./header.js');
+  import('./categories-aside.js');
   const hash = window.location.hash;
+  console.log('Hash' + window.location.hash);
   if (hash) {
-    console.log('// Знаходимо елемент із відповідним ID');
+    // Знаходимо елемент із відповідним ID
     const target = document.querySelector(hash);
+    console.log('Target' + document.querySelector(hash));
     if (target) {
+      // Виконуємо прокрутку до елемента
       console.log('// Виконуємо прокрутку до елемента');
       target.scrollIntoView({ behavior: 'smooth' });
     }
   }
-
-  import('./header.js');
-  import('./categories-aside.js');
 }
 
 const totalPartials = document.querySelectorAll(
