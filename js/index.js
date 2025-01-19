91aae972b57b18b8e5eab6c2c6c8be256723bf3f
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2025-01-19 21:20:02 +0200

    Deleted functionality fot scrolling

--- js/index.js
+++ js/index.js
@@ -1,18 +1,6 @@
 function init() {
   import('./header.js');
   import('./categories-aside.js');
-  const hash = window.location.hash;
-  console.log('Hash' + window.location.hash);
-  if (hash) {
-    // Знаходимо елемент із відповідним ID
-    const target = document.querySelector(hash);
-    console.log('Target' + document.querySelector(hash));
-    if (target) {
-      // Виконуємо прокрутку до елемента
-      console.log('// Виконуємо прокрутку до елемента');
-      target.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
 }
 
 const totalPartials = document.querySelectorAll(
