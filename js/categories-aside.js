011a014af3cf374b998a066a821a21146a0bbb6b
MaksymZymyn <maksym.rizdvjanyj@gmail.com>
2025-01-20 13:40:49 +0200

    Add responsive features for cards

--- js/categories-aside.js
+++
@@ -1,5 +0,0 @@
-function toggleCategoryVisibility() {
-  const categoryList = document.querySelector(".filters__item-category");
-  categoryList.classList.toggle("hidden");
-  categoryList.classList.toggle("visible");
-}
