d574ac4f45f132fda09d2e452f7333bb2258bd0b
MaksymZymyn <maksym.rizdvjanyj@gmail.com>
2025-01-21 14:45:49 +0200

    Add tasks

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -4,3 +4,35 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // Сума трьох чисел
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+// Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  // Якщо число не є додатнім
+  if (num <= 0) {
+    return num;
+  }
+
+  // Якщо число є додатнім
+  return -num;
+}
+
+// Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move(position, roll) {
+  // До початкової позиції додаємо 2 кроки
+  return position + 2 * roll;
+}
+
+// Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet (name, owner) {
+  // Якщо людина, яку вітаємо, є босом
+  if (name === owner) {
+    return 'Hello boss';
+  }
+  
+  // Якщо людина, яку вітаємо, не є босом
+  return 'Hello guest';
+}
+
