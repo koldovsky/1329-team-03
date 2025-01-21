dfa31d077a88789239be818549a6b84b70cdb52d
MaksymZymyn <maksym.rizdvjanyj@gmail.com>
2025-01-21 14:33:46 +0200

    Add Codewars begining

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,6 @@
+// Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // Сума трьох чисел
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
