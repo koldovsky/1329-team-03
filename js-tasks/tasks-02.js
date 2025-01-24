0e9ddd7abc72cedfe235f26258590e58d19de623
MaksymZymyn <maksym.rizdvjanyj@gmail.com>
2025-01-24 11:31:44 +0200

    Add solutions

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -63,16 +63,6 @@ function DNAtoRNA(dna) {
 }
 
 // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-
-// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
-
-// Double Integer    https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
-// Twice as old    https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
-// Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
-// What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
-// Clock    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
-// Is n divisible by x and y    https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
-
 // Find Maximum and Minimum Values of a List
 var min = function (list) {
   return Math.min(...list);
@@ -88,6 +78,17 @@ function min(arr, toReturn) {
     ? Math.min(...arr)
     : arr.indexOf(Math.min(...arr));
 }
+
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+// Double Integer    https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+// Twice as old    https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+// Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+// What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+// Clock    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+// Is n divisible by x and y    https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+
+
 
 // You Can't Code Under Pressure #1
 function doubleInteger(i) {
