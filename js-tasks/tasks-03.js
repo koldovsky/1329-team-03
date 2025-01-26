668fca92e41ac506fa6d14bf7f932d4cd3c96f31
MaksymZymyn <maksym.rizdvjanyj@gmail.com>
2025-01-26 11:50:47 +0200

    Add solutions

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,20 +1,43 @@
 // https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
 function circleCircumference(circle) {
-    return 2 * Math.PI * circle.radius;
-  }
+  return 2 * Math.PI * circle.radius;
+}
 
-  // https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 
-  function giveMeFive(obj){
-    let result = [];
-    for (const value in obj) {
-      if(value.length === 5){
-        result.push(value);
-      }
-      if(obj[value].length === 5){
-        result.push(obj[value]);
-      }
+function giveMeFive(obj) {
+  const arr = [];
+  for (const key in obj) {
+    if (key.length === 5) {
+      arr.push(key);
     }
-    return result;
+    const value = obj[key];
+    if (value.length === 5) {
+      arr.push(value);
+    }
   }
+  return arr;
+}
+
+// Додатковий спосіб - менш ефективно
+function giveMeFive(obj) {
+  const result = [];
+  for (const key in obj) {
+    [key, obj[key]].forEach((k) => {
+      if (k.length == 5) {
+        result.push(k);
+      }
+    });
+  }
+  return result;
+}
+
+// Додатковий спосіб, але метод filter повертає новий масив із відфільтрованими елементами
+function giveMeFive(obj) {
+  const result = [];
+  for (const key in obj) {
+    result.push(key) && result.push(obj[key]);
+  }
+  return result.filter((word) => word.length === 5);
+}
