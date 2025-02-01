a6193e529cd51242714bcec18e1ae7b98a1ca687
Oleksandr Yankovskiy <oankovskij13@gmail.com>
2025-02-01 18:32:36 +0200

    fiexed conflict in index

--- js/index.js
+++ js/index.js
@@ -1,20 +1,10 @@
 function init() {
-<<<<<<< HEAD
-  import("./header.js");
-  import("./index.hero.js");
-  import("./index.deals.js");
-  import("./footer.js");
-  import("./index.categories-aside.accordion.js");
-  import("./index.categories-aside.js");
-=======
   import('./header.js');
   import('./index.hero.js');
   import('./index.deals.js');
   import('./footer.js');
   import('./index.categories.js');
-  import('./index.about-us.js');
   import('./scroll-to-top.js');
->>>>>>> 8cb3181d79014f314a9f23d7e9e84fa47d063731
 }
 
 const totalPartials = document.querySelectorAll(
