8cb3181d79014f314a9f23d7e9e84fa47d063731
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2025-02-01 15:34:39 +0200

    Created js functionality for the scroll button to the top of the site and added some styles

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 function init() {
-  import("./header.js");
-  import("./index.hero.js");
-  import("./index.deals.js");
-  import("./footer.js");
-  import("./index.categories.js");
-  import("./index.about-us.js");
+  import('./header.js');
+  import('./index.hero.js');
+  import('./index.deals.js');
+  import('./footer.js');
+  import('./index.categories.js');
+  import('./index.about-us.js');
+  import('./scroll-to-top.js');
 }
 
 const totalPartials = document.querySelectorAll(
@@ -12,7 +13,7 @@ const totalPartials = document.querySelectorAll(
 ).length;
 let loadedPartialsCount = 0;
 
-document.body.addEventListener("htmx:afterOnLoad", () => {
+document.body.addEventListener('htmx:afterOnLoad', () => {
   loadedPartialsCount++;
   if (loadedPartialsCount === totalPartials) init();
 });
