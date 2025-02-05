c8d2af311bd3eee7d7275fe89162806b16f1d11b
ANDRII MAKOVETSKYI <Shmak@ukr.net>
2025-02-05 11:45:37 +0000

    Precommit

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ function init() {
   import('./index.categories.js');
   import('./scroll-to-top.js');
   import('./swiper.js');
+  import('./index.footer-contact-form.js');
 }
 
 const totalPartials = document.querySelectorAll(
