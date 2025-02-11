1f9d4a5fca54f807fdfaa916b2e892f710d5058c
Vitalina <163914801+missItLady@users.noreply.github.com>
2025-02-11 20:53:11 +0000

    оновлення

--- js/swiper.js
+++ js/swiper.js
@@ -1,19 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".swiper", {
-    slidesPerView: 1,
-    spaceBetween: 15,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      1470: { slidesPerView: 7, spaceBetween: 10 },
-      1150: { slidesPerView: 5, spaceBetween: 10 },
-      900: { slidesPerView: 4, spaceBetween: 10 },
-      700: { slidesPerView: 3, spaceBetween: 10 },
-      500: { slidesPerView: 2, spaceBetween: 10 },
-      0: { slidesPerView: 1, spaceBetween: 10 },
-    },
+      slidesPerView: 1,
+      spaceBetween: 15,
+      loop: true,
+      navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+      },
+      breakpoints: {
+          1470: { slidesPerView: 7, spaceBetween: 10 },
+          1150: { slidesPerView: 5, spaceBetween: 10 },
+          900: { slidesPerView: 4, spaceBetween: 10 },
+          700: { slidesPerView: 3, spaceBetween: 10 },
+          500: { slidesPerView: 2, spaceBetween: 10 },
+          0: { slidesPerView: 1, spaceBetween: 10 },
+      },
   });
 });
