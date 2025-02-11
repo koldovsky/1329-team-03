6e0a7eeea28e67717b59fe82bc18f40b91116b3f
Vitalina <163914801+missItLady@users.noreply.github.com>
2025-02-11 20:59:15 +0000

    fvvw

--- js/swiper.js
+++ js/swiper.js
@@ -1,23 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".swiper", {
-      slidesPerView: 1,
-      spaceBetween: 15,
-      loop: true,
+  const swiper = new Swiper('.swiper', {
+      loop: true, // Зациклення
+      slidesPerView: 3, // Скільки слайдів видно одночасно
+      spaceBetween: 20, // Відстань між слайдами
       navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
       },
       autoplay: {
-          delay: 3000,
+          delay: 3000, // Автоматична прокрутка (3 секунди)
           disableOnInteraction: false,
-      },
-      breakpoints: {
-          1470: { slidesPerView: 7, spaceBetween: 10 },
-          1150: { slidesPerView: 5, spaceBetween: 10 },
-          900: { slidesPerView: 4, spaceBetween: 10 },
-          700: { slidesPerView: 3, spaceBetween: 10 },
-          500: { slidesPerView: 2, spaceBetween: 10 },
-          0: { slidesPerView: 1, spaceBetween: 10 },
       },
   });
 });
