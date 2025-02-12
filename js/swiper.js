121383a0672fd4b87528fc193e8c3aa42a76a190
Vitalina <163914801+missItLady@users.noreply.github.com>
2025-02-12 00:32:24 +0000

    оновлення swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,3 +1,4 @@
+// script.js
 document.addEventListener("DOMContentLoaded", function () {
   const slider = document.querySelector(".slider");
   const prevBtn = document.querySelector(".prev");
@@ -11,43 +12,37 @@ document.addEventListener("DOMContentLoaded", function () {
   function updateSliderPosition() {
     slider.style.transition = "transform 0.5s ease-in-out";
     slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-    console.log("Slider moved to index:", currentIndex);
   }
 
   // Обробник кліку "Вперед"
   nextBtn.addEventListener("click", function () {
-    console.log("Next button clicked");
     if (currentIndex < slides.length - 1) {
       currentIndex++;
-      updateSliderPosition();
     } else {
       currentIndex = 0;
       slider.style.transition = "none";
       slider.style.transform = `translateX(0)`;
       setTimeout(updateSliderPosition, 20);
     }
-    console.log("currentIndex:", currentIndex);
+    updateSliderPosition();
   });
 
   // Обробник кліку "Назад"
   prevBtn.addEventListener("click", function () {
-    console.log("Prev button clicked");
     if (currentIndex > 0) {
       currentIndex--;
-      updateSliderPosition();
     } else {
       currentIndex = slides.length - 1;
       slider.style.transition = "none";
       slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
       setTimeout(updateSliderPosition, 20);
     }
-    console.log("currentIndex:", currentIndex);
+    updateSliderPosition();
   });
 
   // Оновлення slideWidth при зміні розміру вікна
   window.addEventListener("resize", function () {
     slideWidth = slides[0].offsetWidth + 23;
     updateSliderPosition();
-    console.log("Window resized, new slideWidth:", slideWidth);
   });
 });
